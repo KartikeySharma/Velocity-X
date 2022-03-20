e1ce51e664bde306f7ebe4bdb18db0434abbb1f9
Medha Khatri <81421540+medhhaa@users.noreply.github.com>
2022-03-20 15:56:54 +0530

    update

--- lib/screens/UserProfile/profile.dart
+++ lib/screens/UserProfile/profile.dart
@@ -6,22 +6,20 @@ class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-     
     return Container(
-    
-      child: Scaffold(
-        backgroundColor: Color.fromRGBO(36, 36, 36, 1.0),
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          title: Text('User Information'),
-          centerTitle: true,
-          backgroundColor: Color.fromRGBO(36, 36, 36, 1.0),
+        child: Scaffold(
+      backgroundColor: Color.fromRGBO(36, 36, 36, 1.0),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
+        title: Text('User Information'),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(36, 36, 36, 1.0),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
           padding: const EdgeInsets.fromLTRB(20, 70, 10, 70),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,11 +28,25 @@ class Profile extends StatelessWidget {
                 height: 140.0,
                 width: 340.0,
               ),
-              Icon(
-                Icons.account_circle_sharp,
-                color: Colors.pink,
-                size: 40.0,
-                semanticLabel: 'Text to announce in accessibility modes',
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.account_circle_sharp,
+                      color: Color.fromARGB(255, 235, 171, 192),
+                      size: 70.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Demo_user',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 15,
